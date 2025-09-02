4101eb52c8af691f757ed87a0d7dbcae70019889
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-09-02 21:08:18 +0530

    added prisma connection code

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+
+// if any edge there is a connection then use it and if there is not a connection then create one. so that we will not be outoff limit
+const prismaClientSingleTon = () => {
+    return new PrismaClient();
+}
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleTon();
+// if 'globalForPrisma.prisma' does not exist then run this code 'prismaClientSingleTon()'
+
+export default prisma;
+
+// this line prevent during development reload would create a new PrismaClient instance
+if(process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
